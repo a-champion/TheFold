d10beda1777542e2561903c14e42804e0ef02d0a
a-champion <a.champion1506@gmail.com>
2023-01-13 03:28:32 -0700

    spring boot for server/api

---
+++ capstoneback/src/main/java/com/accapstone/capstoneback/CapstonebackApplication.java
@@ -0,0 +1,13 @@
+package com.accapstone.capstoneback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstonebackApplication.class, args);
+	}
+
+}
---
+++ capstoneback/src/test/java/com/accapstone/capstoneback/CapstonebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accapstone.capstoneback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
